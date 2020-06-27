9f41d6c064237adcfc3c9af2f9e8e03896003b6f
Hasan <7asanalbusairi@gmail.com>
2020-06-27 20:32:33 +0300

    تم عمل التمرين

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,34 +1,47 @@
 // السؤال الأول
 // 1
+let name=("hasan");
 
 // 2
-
+let age=("17");
 // 3 and 4
-
+console.log("انا"+"  "+name+"  "+"وعمري"+" "+age);
 // 5 and 6
-
+age *=5
+console.log(age);
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
-
+const hobby=["swimming","reading"]
 // 2
-
+hobby.push("17");
 // 3
+hobby.pop();
+console.log(hobby);
 
 // 4
-
+let web={
+    language1:"javascript"
+};
 // 5
-
+let ta=["Hussain","Moudhi","Rahaf","Nancy"];
 // 6
+ web = {
+    language: "JavaScript",
 
+    name: "web",
+
+    ta:["Moudhi", "Hussien", "Rahaf", "Nancy"]
+   
+};
 // 7
-
+console.log("Hussain");
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // البونص
 // 1
-
+Techers = "Live is very good";
 // 2
-
+console.log(Techers)
 // console results => console.png
